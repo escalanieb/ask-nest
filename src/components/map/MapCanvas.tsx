055851305a46fe9c988a15db5c1730c25ddc3ea1
Ronniel Sajol <sajolronniel28@gmail.com>
2026-09-09 14:30:10 +0800

    fix:modified map

--- src/components/map/MapCanvas.tsx
+++ src/components/map/MapCanvas.tsx
@@ -1520,8 +1520,9 @@ export default function MapCanvas() {
         attributionControl={true}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maxZoom={19}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <GeoJSON
           ref={geoJsonRef}
